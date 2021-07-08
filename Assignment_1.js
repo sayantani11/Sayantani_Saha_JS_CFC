6af6a019aff1c7df2ed7b82689c630d06c342b99
Sayantani Saha <75435974+sayantani11@users.noreply.github.com>
2021-07-09 04:26:57 +0530

    Create Assignment_1.js

--- Assignment_1.js
+++ Assignment_1.js
@@ -100,3 +100,81 @@ while (row<=5){
     process.stdout.write("\n");
     row ++;
 }
+
+//Question 7b
+let n = 5
+let i = 1
+while(i<=n){
+    let j=1
+    while(j<=i){
+        process.stdout.write(j + " ")
+        j++
+    }
+    i++
+    process.stdout.write("\n")
+}
+
+//Question 7f
+let n = 5
+let row = 0
+let rowMirror = 0
+while(rowMirror< 2*n-1){
+    let col = 0
+    let colMirror= 0
+    while(colMirror< 2*n-1){
+        if(col<n-row-1){
+            process.stdout.write("* ")
+        }
+        else{
+            process.stdout.write("  ")
+        }
+        if(colMirror<n-1){
+            col++
+        }
+        else{
+            col--
+        }
+        colMirror++
+    }
+    process.stdout.write("\n");
+    if(rowMirror<n-1){
+        row++
+    }
+    else{
+        row--
+    }
+    rowMirror++
+}
+
+//Question 7 h
+let n = 5
+let row = 0
+let rowMirror = 0
+while(rowMirror< 2*n-1){
+    let col = 0
+    let colMirror= 0
+    while(colMirror< 2*n-1){
+        if(col<n-row){
+            process.stdout.write("* ")
+        }
+        else{
+            process.stdout.write("  ")
+        }
+        if(colMirror<n-1){
+            col++
+        }
+        else{
+            col--
+        }
+        
+        colMirror++
+    }
+    process.stdout.write("\n");
+    if(rowMirror<n-1){
+        row++;
+    }
+    else{
+        row--;
+    }
+    rowMirror++;
+}
