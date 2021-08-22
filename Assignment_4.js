357cae8088395e9c452dc1bbfd9b4a2906ae2405
Sayantani Saha <75435974+sayantani11@users.noreply.github.com>
2021-08-23 03:56:03 +0530

    Update Assignment_4.js

--- Assignment_4.js
+++ Assignment_4.js
@@ -1,1 +1,50 @@
+// Question 2
+function sum(n){
+    if(n==0){
+        return 0;
+    }
+
+    return n + sum(n-1);
+}
+
+
+// Question 3
+function targetIdx(arr, target, index){
+    if(index < 0){
+        return -1;
+    }
+
+    if(arr[index] == target){
+        return index;
+    }
+
+    targetIdx(arr, target, index-1);
+}
+
+
+// Question 4
+function arrofIdx(arr, target, index=0){
+    idx = [];
+    
+    if(index == arr.length - 1){
+        return idx;
+    }
+
+    if(arr[index] == target){
+        idx.push(index);
+    }
+
+    arrofIdx(arr, target, index+1);
+}
+
+
+// Question 5
+function reverse(arr, start, end){
+    if(start >= end){
+        return arr;
+    }
+
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    reverse(arr, start+1, end-1);
+}
 
